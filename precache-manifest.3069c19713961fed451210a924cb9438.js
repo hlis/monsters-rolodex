7b09424b1b7b6f1f6f6249495571324e7345b871
jonathan1531 <jonathan.find@gmail.com>
2020-03-23 20:01:20 -0500

    Updates

---
+++ precache-manifest.3069c19713961fed451210a924cb9438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712e5387ed0a0f2f6e4f359cb0fcd752",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d293ce6b79f3754c516",
+    "url": "/monsters-rolodex/static/css/main.87d3f4f7.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d293ce6b79f3754c516",
+    "url": "/monsters-rolodex/static/js/main.ed7200b6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
